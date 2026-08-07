00ce913e037d2d84dbf1b8f338de0e7f46d0546c
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2026-08-07 13:52:52 +0200

    Test

--- VTKHDF.jl/docs/make.jl
+++ VTKHDF.jl/docs/make.jl
@@ -2,6 +2,6 @@ using Documenter, Example
 
 makedocs(sitename="VTKHDF.jl")
 
-if get(ENV, "CI", "") != "" && (get(ENV, "GITHUB_REF", "") == "refs/heads/main" || startswith(get(ENV, "GITHUB_REF", ""), "refs/tags/"))
+# if get(ENV, "CI", "") != "" && (get(ENV, "GITHUB_REF", "") == "refs/heads/main" || startswith(get(ENV, "GITHUB_REF", ""), "refs/tags/"))
     deploydocs(repo = "github.com/AhmedSalih3d/VTKHDF.jl.git")
-end
+# end
