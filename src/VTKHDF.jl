b96ece9cf8356a25e1c4ef42f394c53224532a7a
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2026-08-09 01:09:04 +0200

    Use do setup to ensure all savefile handling is done properly

--- src/VTKHDF.jl
+++ src/VTKHDF.jl
@@ -25,46 +25,42 @@ Each entry in `variable_names` must have a matching array in `point_data`.
 """
 function SaveVTKHDF(filepath, points, variable_names = String[], point_data...)
   @assert length(variable_names) == length(point_data) "Same number of variable_names as point_data arrays is necessary"
-  io = h5open(filepath, "w")
-  gtop = HDF5.create_group(io, "VTKHDF")
+  
+    h5open(filepath, "w") do io
+    gtop = HDF5.create_group(io, "VTKHDF")
 
-  HDF5.attrs(gtop)["Version"] = [2, 8]
-  _write_ascii_attribute(gtop, "Type", "PolyData")
+    HDF5.attrs(gtop)["Version"] = [2, 8]
+    _write_ascii_attribute(gtop, "Type", "PolyData")
 
-  # Points
-  np = length(points)
-  gtop["NumberOfPoints"] = [np]
-  gtop["Points"] = reinterpret(reshape, eltype(eltype(points)), points)
+    # Points
+    np = length(points)
+    gtop["NumberOfPoints"] = [np]
+    gtop["Points"] = reinterpret(reshape, eltype(eltype(points)), points)
 
-  # Point data
-  let g = HDF5.create_group(gtop, "PointData")
-    for i ∈ eachindex(variable_names)
-      g[variable_names[i]] = reinterpret(reshape, eltype(eltype(point_data[i])), point_data[i])
+    # Point data
+    let g = HDF5.create_group(gtop, "PointData")
+      for i ∈ eachindex(variable_names)
+        g[variable_names[i]] = reinterpret(reshape, eltype(eltype(point_data[i])), point_data[i])
+      end
     end
-    close(g)
-  end
 
-  # Vertices: 1 point per cell
-  let g = HDF5.create_group(gtop, "Vertices")
-    g["NumberOfCells"] = [np]
-    g["NumberOfConnectivityIds"] = [np]
-    g["Connectivity"] = collect(0:(np-1))
-    g["Offsets"] = collect(0:np)
-    close(g)
-  end
+    # Vertices: 1 point per cell
+    let g = HDF5.create_group(gtop, "Vertices")
+      g["NumberOfCells"] = [np]
+      g["NumberOfConnectivityIds"] = [np]
+      g["Connectivity"] = collect(0:(np-1))
+      g["Offsets"] = collect(0:np)
+    end
 
-  # Empty groups for unused cell types
-  for type ∈ ("Lines", "Polygons", "Strips")
-    gempty = HDF5.create_group(gtop, type)
-    gempty["NumberOfCells"] = [0]
-    gempty["NumberOfConnectivityIds"] = [0]
-    gempty["Connectivity"] = Int[]
-    gempty["Offsets"] = [0]
-    close(gempty)
+    # Empty groups for unused cell types
+    for type ∈ ("Lines", "Polygons", "Strips")
+      gempty = HDF5.create_group(gtop, type)
+      gempty["NumberOfCells"] = [0]
+      gempty["NumberOfConnectivityIds"] = [0]
+      gempty["Connectivity"] = Int[]
+      gempty["Offsets"] = [0]
+    end
   end
-
-  close(gtop)
-  close(io)
 end
 
 end
